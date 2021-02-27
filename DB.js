17cec9966be88c5740f2aad1d1c602b4be6f374c
Fernando Hottum <Fernando.Hottum@thomsonreuters.com>
2021-02-27 05:32:12 -0300

    Sistema funcionando sem id especifico para as entidades

---
+++ DB.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDyFhwo0QCLMgWd0exzNSN0l_OfrTaZnOk",
+    authDomain: "curso-banco-de-dados-8fd6f.firebaseapp.com",
+    databaseURL: "https://curso-banco-de-dados-8fd6f-default-rtdb.firebaseio.com",
+    projectId: "curso-banco-de-dados-8fd6f",
+    storageBucket: "curso-banco-de-dados-8fd6f.appspot.com",
+    messagingSenderId: "1063137830200",
+    appId: "1:1063137830200:web:d9d9334d2e0aeb2b840dcd"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
---
+++ buscas.js
@@ -0,0 +1,49 @@
+document.getElementById("buscaUm").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaDois").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaTres").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaQuatro").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaCinco").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaSeis").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaSete").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaOito").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaNove").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
+
+document.getElementById("buscaDez").addEventListener("click", function () {
+    var resultado = document.getElementById("resultados");
+    resultado.innerHTML = '';
+});
